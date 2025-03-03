c95487462ad1e632ed14ef996e4278c8085b8ae5
Adrián Estévez <100156445+Adr1Est@users.noreply.github.com>
2025-03-03 13:13:18 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -7,5 +7,18 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let who = ['El perro', 'Mi dinosaurio', 'El cartero', 'Mi pájaro'];
+  let action = ['se comió', 'meó', 'aplastó', 'rompió'];
+  let what = ['mis deberes', 'mi móvil', 'el coche'];
+  let when = ['antes de clase', 'mientras dormía', 'mientras hacía ejercicio', 'durante la comida', 'mientras hacía la compra'];
+  let resultado = "";
+
+  let randomNum = (array) => {
+    let numero = Math.floor(Math.random() * array.length);
+    return numero;
+  }
+
+  resultado = who[randomNum(who)] + " " + action[randomNum(action)] + " " + what[randomNum(what)] + " " + when[randomNum(when)] + ".";
+  document.querySelector("#excusa").innerHTML = resultado;
 };
+
